9875e1551e4a353067663254671c868273124e80
weaverb <bryanweaver@outlook.com>
2015-03-28 13:10:17 -0500

    changed background to black

--- core/src/com/bryanweaver/samplegame/MyGdxGame.java
+++ core/src/com/bryanweaver/samplegame/MyGdxGame.java
@@ -30,7 +30,7 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         if(ScreenManager.getCurrentScreen() != null){
